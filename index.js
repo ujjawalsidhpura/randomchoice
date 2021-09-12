522527cd2dc5ad6cd4f9eedfd0c53c20e4a4f43e
Ujjawal Sidhpura <85900088+ujjawalsidhpura@users.noreply.github.com>
2021-09-12 13:31:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSlect();
+  }
+})
+
+//Main Function
+
+function createTags(input) {
+  const tags = input.split(',').filter(tag => tag.trim()).map(tag => tag.trim());
+
+  tagsEl.innerHTML = '';
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl)
+  })
+}
+
+function randomSlect() {
+  const times = 30;
+
+  //Animation of highlight and unhighlight
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    }, (100));
+
+  }, 100);
+
+
+  //This function takes care of giving out final picked tag
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag)
+    }, 100);
+  }, times * 100);
+}
+
+//Helper Functions 
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
+
+
